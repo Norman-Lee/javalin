7c7ba2c3a2c3d21cab061e82e0d70052753d8da3
Norman Lee <norman.lee91@gmail.com>
2017-12-02 20:19:30 -0600

    Added embedded lambda server factory and server related server class

--- src/main/java/io/javalin/embeddedserver/AWSLambda/EmbeddedAWSLambdaFactory.kt
+++ src/main/java/io/javalin/embeddedserver/AWSLambda/EmbeddedAWSLambdaFactory.kt
@@ -1,7 +1,17 @@
 package io.javalin.embeddedserver.AWSLambda
 
+import io.javalin.core.JavalinServlet
+import io.javalin.embeddedserver.EmbeddedServer
 import io.javalin.embeddedserver.EmbeddedServerFactory
+import io.javalin.embeddedserver.StaticFileConfig
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.util.thread.QueuedThreadPool
 
-class EmbeddedAWSLambdaFactory(lambdaServer: () -> ) : EmbeddedServerFactory {
-    private val
+
+class EmbeddedAWSLambdaFactory : EmbeddedServerFactory {
+    // Need to add static resource config handling
+    override fun create(javalinServlet: JavalinServlet, staticFileConfig: StaticFileConfig?): EmbeddedServer {
+        return EmbeddedAWSLambdaServer(javalinServlet)
+    }
+
 }
--- src/main/java/io/javalin/embeddedserver/AWSLambda/EmbeddedAWSLambdaServer.kt
+++ src/main/java/io/javalin/embeddedserver/AWSLambda/EmbeddedAWSLambdaServer.kt
@@ -3,30 +3,32 @@ package io.javalin.embeddedserver.AWSLambda
 import com.amazonaws.services.lambda.runtime.Context
 import io.javalin.core.JavalinServlet
 import io.javalin.embeddedserver.EmbeddedServer
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import org.slf4j.LoggerFactory
 
-class EmbeddedAWSLambdaServer(private val javalinServlet: JavalinServlet) : EmbeddedServer {
 
-    fun handleRequest(req : APIGatewayProxyRequestEvent, context: Context) : APIGatewayProxyResponseEvent {
-        val res = APIGatewayProxyResponseEvent()
-        javalinServlet.service(req, res)
-        return res
-    }
+class EmbeddedAWSLambdaServer(private val javalinServlet: JavalinServlet) : EmbeddedServer{
+    private val log = LoggerFactory.getLogger(EmbeddedServer::class.java)
 
     override fun start(port: Int): Int {
+        log.info("Starting Javalin Server, ignoring the port")
+
+//        javalinServlet.service(req, res)
+
         return 0
     }
 
     override fun stop() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        log.info("Server stopped, nothing to do")
     }
 
     override fun activeThreadCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        log.info("Running on lambda, since it is one event per container, return value is always 1")
+        return 1
     }
 
     override fun attribute(key: String): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        log.info("No attributes as this is a lambda")
+        return "No Attributes"
     }
+
 }
